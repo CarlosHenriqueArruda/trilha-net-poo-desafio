ccc2670fdb5b4bf7a1780306877392a8f6ee397e
carloshenriquearruda <carloshenriquearruda82@gmail.com>
2023-12-11 17:20:03 -0300

    desafio muito facil avanade

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123", modelo:"Antigo",imei:"123",memoria:32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero:"321", modelo:"Novo",imei:"321",memoria:8);
+iphone.Ligar();
+iphone.InstalarAplicativo("LinkedIn");
